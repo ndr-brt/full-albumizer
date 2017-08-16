4ec507f676e306143f361183b89e3d484b0774a4
Andrea Bertagnolli <andrea.bertagnolli@paros.srl>
2017-08-16 17:10:24 +0200

    Create video with the first image contained in the folder

--- src/main/java/FullAlbumizer.java
+++ src/main/java/FullAlbumizer.java
@@ -6,11 +6,15 @@ import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.function.Predicate;
 
 import static io.humble.video.Codec.ID.CODEC_ID_MPEG2VIDEO;
 import static io.humble.video.PixelFormat.Type.PIX_FMT_YUV420P;
 
 public class FullAlbumizer {
+
+    private static final Predicate<File> JPEG_FILE = file -> file.getName().endsWith(".jpg");
 
     /*
      good example:
@@ -18,14 +22,24 @@ public class FullAlbumizer {
       */
 
     public static void main(String[] args) throws IOException, InterruptedException {
-        final Muxer muxer = Muxer.make("/tmp/tmpmovie.mpeg", null, null);
+
+        String folderPath = "/home/andrea/Music/Rituals, The - 2009 - Celebrate Life";
+
+        File folder = new File(folderPath);
+        File album = new File(folder, "album.mpeg");
+
+        File image = Arrays.stream(folder.listFiles())
+                .filter(JPEG_FILE).findFirst().get();
+
+        final Muxer muxer = Muxer.make(album.getAbsolutePath(), null, null);
         final Rational framerate = Rational.make(1, 1);
         final Codec codec = Codec.findEncodingCodec(CODEC_ID_MPEG2VIDEO);
+        final BufferedImage bufferedImage = ImageIO.read(image);
 
         PixelFormat.Type pixelFormat = PIX_FMT_YUV420P;
         Encoder encoder = Encoder.make(codec);
-        encoder.setWidth(787);
-        encoder.setHeight(787);
+        encoder.setWidth(bufferedImage.getWidth());
+        encoder.setHeight(bufferedImage.getHeight());
         encoder.setPixelFormat(pixelFormat);
         encoder.setTimeBase(framerate);
 
@@ -35,20 +49,18 @@ public class FullAlbumizer {
 
         muxer.open(null, null);
 
-        MediaPictureConverter converter = null;
         final MediaPicture picture = MediaPicture.make(
                 encoder.getWidth(),
                 encoder.getHeight(),
                 pixelFormat);
         picture.setTimeBase(framerate);
 
+        MediaPictureConverter converter = MediaPictureConverterFactory.createConverter(bufferedImage, picture);
+
         final MediaPacket packet = MediaPacket.make();
         for (int i = 0; i < 100 / framerate.getDouble(); i++) {
-            final BufferedImage image = ImageIO.read(new File("./src/main/resources/slack.jpg"));
 
-            converter = MediaPictureConverterFactory.createConverter(image, picture);
-
-            converter.toPicture(picture, image, i);
+            converter.toPicture(picture, bufferedImage, i);
 
             do {
                 encoder.encode(packet, picture);
