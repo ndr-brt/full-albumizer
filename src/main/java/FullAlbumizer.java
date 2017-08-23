bc2ce41f51daac582ced532da063be92fa910ed3
Andrea Bertagnolli <andrea.bertagnolli@paros.srl>
2017-08-23 13:52:09 +0200

    Add audio to video

--- src/main/java/FullAlbumizer.java
+++ src/main/java/FullAlbumizer.java
@@ -7,14 +7,17 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.List;
 import java.util.function.Predicate;
 
 import static io.humble.video.Codec.ID.CODEC_ID_MPEG2VIDEO;
 import static io.humble.video.PixelFormat.Type.PIX_FMT_YUV420P;
+import static java.util.stream.Collectors.toList;
 
 public class FullAlbumizer {
 
     private static final Predicate<File> JPEG_FILE = file -> file.getName().endsWith(".jpg");
+    private static final Predicate<File> MP3_FILE = file -> file.getName().endsWith(".mp3");
 
     /*
      good example:
@@ -33,10 +36,10 @@ public class FullAlbumizer {
 
         final Muxer muxer = Muxer.make(album.getAbsolutePath(), null, null);
         final Rational framerate = Rational.make(1, 1);
-        final Codec codec = Codec.findEncodingCodec(CODEC_ID_MPEG2VIDEO);
         final BufferedImage bufferedImage = ImageIO.read(image);
 
         PixelFormat.Type pixelFormat = PIX_FMT_YUV420P;
+        final Codec codec = Codec.findEncodingCodec(CODEC_ID_MPEG2VIDEO);
         Encoder encoder = Encoder.make(codec);
         encoder.setWidth(bufferedImage.getWidth());
         encoder.setHeight(bufferedImage.getHeight());
@@ -45,9 +48,6 @@ public class FullAlbumizer {
 
         encoder.open(null, null);
 
-        muxer.addNewStream(encoder);
-
-        muxer.open(null, null);
 
         final MediaPicture picture = MediaPicture.make(
                 encoder.getWidth(),
@@ -57,23 +57,62 @@ public class FullAlbumizer {
 
         MediaPictureConverter converter = MediaPictureConverterFactory.createConverter(bufferedImage, picture);
 
-        final MediaPacket packet = MediaPacket.make();
-        for (int i = 0; i < 100 / framerate.getDouble(); i++) {
+        List<File> songs = Arrays.stream(folder.listFiles())
+                .filter(MP3_FILE).collect(toList());
 
-            converter.toPicture(picture, bufferedImage, i);
+        Demuxer audioMuxer = Demuxer.make();
+        audioMuxer.open(songs.get(0).getAbsolutePath(), null, false, true, null, null);
+        DemuxerStream stream = audioMuxer.getStream(0);
+        Decoder audioDecoder = stream.getDecoder();
+        Codec audioCodec = Codec.findEncodingCodec(audioDecoder.getCodecID());
+        Encoder aEncoder = Encoder.make(audioCodec);
+        aEncoder.setSampleRate(audioDecoder.getSampleRate());
+        aEncoder.setChannelLayout(audioDecoder.getChannelLayout());
+        aEncoder.setSampleFormat(audioDecoder.getSampleFormat());
 
+        audioDecoder.open(null, null);
+
+        MediaAudio samples = MediaAudio.make(
+                audioDecoder.getFrameSize(),
+                audioDecoder.getSampleRate(),
+                audioDecoder.getChannels(),
+                audioDecoder.getChannelLayout(),
+                audioDecoder.getSampleFormat()
+        );
+
+        aEncoder.open(null, null);
+
+        muxer.addNewStream(encoder);
+        muxer.addNewStream(aEncoder);
+
+        muxer.open(null, null);
+
+        final MediaPacket audioPacket = MediaPacket.make();
+        final MediaPacket videoPacket = MediaPacket.make();
+        converter.toPicture(picture, bufferedImage, 0);
+        encoder.encode(videoPacket, picture);
+        while(audioMuxer.read(audioPacket) >= 0) {
+            int offset = 0;
+            int bytesRead = 0;
             do {
-                encoder.encode(packet, picture);
-                if (packet.isComplete())
-                    muxer.write(packet, false);
-            } while (packet.isComplete());
+                bytesRead += audioDecoder.decode(samples, audioPacket, offset);
+
+                aEncoder.encodeAudio(audioPacket, samples);
+
+                offset += bytesRead;
+
+                if (audioPacket.isComplete())
+                    muxer.write(audioPacket, false);
+
+            } while (offset < audioPacket.getSize());
         }
 
         do {
-            encoder.encode(packet, null);
-            if (packet.isComplete())
-                muxer.write(packet,  false);
-        } while (packet.isComplete());
+            encoder.encodeVideo(audioPacket, null);
+            aEncoder.encodeAudio(videoPacket, null);
+            if (audioPacket.isComplete())
+                muxer.write(audioPacket,  false);
+        } while (audioPacket.isComplete());
 
         muxer.close();
 
